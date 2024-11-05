e821e8029e1793614eda6a09c0c3c41f66212d38
Eduarda Silva Neves <86806534+eduardasneves18@users.noreply.github.com>
2024-11-04 22:31:03 -0300

    Fase 1

--- src/app/api/transactions/route.ts
+++ src/app/api/transactions/route.ts
@@ -2,17 +2,13 @@ import { open } from 'sqlite';
 import sqlite3 from 'sqlite3';
 import _ from 'lodash'
 
-// Abrindo conexão com o banco de dados
 const dbPromise = open({
-  filename: './database.db', // Nome do arquivo do banco de dados
+  filename: './database.db',
   driver: sqlite3.Database,
 });
 
-// Funções para trabalhar com transações
-
 export const revalidate = 60;
 
-// Função para inicializar o banco de dados
 async function initializeDatabase() {
 const db = await dbPromise;
 await db.exec(`
@@ -25,33 +21,28 @@ await db.exec(`
 `);
 }
 
-// Criar uma nova transação
 export async function createTransaction(transaction: { amount: number; type: string; description: string }) {
   const db = await dbPromise;
   const { amount, type, description } = transaction;
   await db.run('INSERT INTO transactions (amount, type, description) VALUES (?, ?, ?)', [amount, type, description]);
 }
 
-// Ler todas as transações
 export async function getTransactions() {
   const db = await dbPromise;
   return await db.all('SELECT * FROM transactions');
 }
 
-// Atualizar uma transação
 export async function updateTransaction(id: string, updatedTransaction: { amount: number; type: string; description: string }) {
   const db = await dbPromise;
   const { amount, type, description } = updatedTransaction;
   await db.run('UPDATE transactions SET amount = ?, type = ?, description = ? WHERE id = ?', [amount, type, description, id]);
 }
 
-// Remover uma transação
 export async function deleteTransaction(id: number | string) {
   const db = await dbPromise;
   await db.run('DELETE FROM transactions WHERE id = ?', [id]);
 }
 
-// API para trabalhar com transações
 export async function GET(req: Request) {
 
   try {
@@ -67,44 +58,33 @@ export async function GET(req: Request) {
   }
 }
 
-// Exemplo de como poderia ser um endpoint para criar uma transação
+
 export async function POST(req: Request) {
   const transaction = await req.json();
   await createTransaction(transaction);
   return new Response(JSON.stringify('Transaction created'), { status: 201 });
 }
 
-// Exemplo de como poderia ser um endpoint para atualizar uma transação
-export async function PUT(req: Request) {
-   // Parse the request body
-   const transaction = await req.json();
-  
-   // Create a URL object to easily access query parameters
-   const url = new URL(req.url);
-   
-   // Get query parameters
-   const id = url.searchParams.get('id'); // Replace 'id' with the actual query parameter name if needed
 
-   if (!_.isEmpty(id)) {
-     updateTransaction(id!, transaction);
-   }
+export async function PUT(req: Request) {
+  const transaction = await req.json();
+  const url = new URL(req.url);
+  const id = url.searchParams.get('id'); 
+
+  if (!_.isEmpty(id)) {
+    updateTransaction(id!, transaction);
+  }
  
   return new Response(JSON.stringify('Transaction updated'), { status: 200 });
 }
 
-// Exemplo de como poderia ser um endpoint para remover uma transação
 export async function DELETE(req: Request) {
-  
-   // Create a URL object to easily access query parameters
-   const url = new URL(req.url);
-   
-   // Get query parameters
-   const id = url.searchParams.get('id'); // Replace 'id' with the actual query parameter name if needed
+  const url = new URL(req.url);
+  const id = url.searchParams.get('id'); 
 
-   if (!_.isEmpty(id)) {
+  if (!_.isEmpty(id)) {
     await deleteTransaction(id!);
-
-   }
+  }
 
   return new Response(JSON.stringify('Transaction deleted'), { status: 200 });
 }
